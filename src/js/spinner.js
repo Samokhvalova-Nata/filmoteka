badaf6498795d007c493e5ec2339c6d8182b1f78
Ivan Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-04-14 19:55:03 +0200

    sss
    отлт

---
+++ src/js/spinner.js
@@ -0,0 +1,47 @@
+import { Spinner } from 'spin.js';
+
+window.onload = function () {
+  const preloaderWebsite = document.querySelector('.spinner');
+  setTimeout(function () {
+    preloaderWebsite.classList.add('is-closed');
+  }, 3000);
+};
+
+//spinner async
+const gallery = document.querySelector('.gallery-spin');
+console.log(gallery);
+gallery.innerHTML = `<div class="spinner-backdrop"></div>`;
+
+const opts = {
+  lines: 12, // The number of lines to draw
+  length: 25, // The length of each line
+  width: 19, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1.2, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#fe5b5b', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '500px', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
+};
+
+let target = document.getElementById('spinContainer');
+const backdrop = document.querySelector('.spinner-backdrop');
+const spinner = new Spinner(opts);
+function playSpinner() {
+  backdrop.classList.remove('is-closed');
+  spinner.spin(target);
+}
+function stopSpinner() {
+  backdrop.classList.add('is-closed');
+  spinner.stop();
+}
+export { playSpinner, stopSpinner };
