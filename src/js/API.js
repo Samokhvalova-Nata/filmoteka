c3f73fb33fc693a69ab9dc1aacff6f4f8bc4d1bf
Oleksy Lykov <lukov_jr@ukr.net>
2023-04-15 17:02:23 +0300

    Update API.js

--- src/js/API.js
+++ src/js/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export class FetchThemoviedbAPI {
+export default class FetchThemoviedbAPI {
     BASE_URL = 'https://api.themoviedb.org/3/';
     API_KEY = '169863a84bc27c731fc45c45dd4a4a7e';
 
@@ -13,7 +13,7 @@ export class FetchThemoviedbAPI {
   async fetchMovie() {
      try {
       
-      return await axios.get(`${this.BASE_URL}`, {
+      const url =  (`${this.BASE_URL}`, {
             params: {
                 api_key: this.API_KEY,
                 q: this.query,
@@ -21,8 +21,46 @@ export class FetchThemoviedbAPI {
                 language: 'en-US',
             }
         });
+     const response = await axios.get(url);
+     return response.data;
     } catch (error) {
       throw new Error(error.message);
       };
     };
+
+    async fetchTrendMovies() {
+        try {
+          const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}&language=${this.lang}`;
+          const response = await axios.get(url);
+          return response.data;
+        } catch (error) {
+            throw new Error(error.message);
+        };
+      };
+
+
+    async fetchMovieDetails(id) {
+      try {
+        const url = `${BASE_URL}movie/${id}?api_key=${API_KEY}&language=${this.lang}`;
+        const response = await axios.get(url);
+        return response.data;
+      } catch (error) {
+          throw new Error(error.message);
+      };
+     };
+
+
+      incrementPage() {
+        this.page += 1;
+      };
+
+      decrementPage() {
+        this.page -= 1;
+      };
+
+      resetPage() {
+        this.page = 1;
+      };
 };
+
+
