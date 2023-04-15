ee2026c67169a61bc28b48e78a1ac48d70fc2964
Nataliia Kurylo <kurilo.nata17@gmail.com>
2023-04-15 15:00:31 +0300

    Add pagination and pop-films

---
+++ src/js/pop-films-loading.js
@@ -0,0 +1,42 @@
+// // FT-07 Реалізувати підвантаження популярних фільмів на головну (першу) сторінку
+import { poster_sizes } from './fetchFromTheMovieDB';
+import { pagination } from './pagination.js';
+import { fetchPopMovies } from './fetchFromTheMovieDB';
+import { genres } from './genres';
+// import Notiflix from 'notiflix';
+
+
+console.log('poster_sizes:', poster_sizes);//TODO:
+// !!!!!!!!!!!!!!------ Змінити на (коли Вадим додасть в розмітку ul) ------!!!!!!!!
+// // const galleryListEl = document.querySelector('.film__list');
+const galleryListEl = document.querySelector('.gallery');
+
+//initial fetch for 1st page
+handlePageBtnClick(1); //pagination._currentPage = 1
+
+pagination.on('afterMove', () => handlePageBtnClick(pagination._currentPage));
+
+async function handlePageBtnClick(page) {
+    try {
+      const data = await fetchPopMovies(page);
+      galleryListEl.innerHTML = createGalleryCards(data.results, poster_sizes[3]);
+      pagination.setTotalItems(data.total_results);
+    } catch (error) { //TODO: notification with Notiflix.error
+        console.log('ERROR! ',error);
+    }
+}
+
+function createGalleryCards(results, poster_size) {
+    return results.map(({ poster_path, title, genre_ids, release_date}) => {
+        return `
+        <li class="film__item"> 
+            <a class="film__item-link link" href="#"> 
+                <img class="film__poster" src="https://image.tmdb.org/t/p/${poster_size}${poster_path}" alt="poster to this movie" /> 
+                <div class="film__info"> 
+                     <h2 class="film__name">${title.slice(0, 30)}</h2> 
+                     <p class="film__ganres">${genres.getSome(genre_ids)} | ${release_date.slice(0, 4)}</p> 
+                </div> </a>
+         </li>
+    `}).join('');
+}
+
