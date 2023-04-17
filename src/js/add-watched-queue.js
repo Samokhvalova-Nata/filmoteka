4b6266b89ab76ea328c25565b03a26116e4e6748
Valerii Pometun <vmpometun@gmail.com>
2023-04-17 16:12:56 +0300

    add to local storage func

--- src/js/add-watched-queue.js
+++ src/js/add-watched-queue.js
@@ -1,30 +1,78 @@
+const API_KEY = '7fc57a32bb8b4747bafc97bb7301e33f';
+const BASE_URL = 'https://api.themoviedb.org';
 localStorage.removeItem('queue');
 localStorage.removeItem('watched');
-const id = 55;
+const id = 76600;
 
-addWatchedEl = document.querySelector('.modal__button-watched');
-addQueueEl = document.querySelector('.modal__button-queue');
+638974;
+736790;
+830896;
+816904;
+493529;
+603692;
+
+const addWatchedEl = document.querySelector('.modal__button-watched');
+const addQueueEl = document.querySelector('.modal__button-queue');
+const galleryCard = document.querySelector('.film__gallery');
 
 addWatchedEl.addEventListener('click', addFilmToWatchedStorage);
 addQueueEl.addEventListener('click', addFilmToQueueStorage);
+galleryCard.addEventListener('click', checkStorageKeys);
 
 function addFilmToWatchedStorage() {
-  //   let watchedStorage = JSON.parse(localStorage.getItem('watched'));
-  localStorage.setItem('watched', id);
+  const fromLocal = localStorage.getItem('watched');
+  localStorage.setItem(
+    'watched',
+    JSON.stringify(checkLocaleStorage(fromLocal))
+  );
 }
 
 function addFilmToQueueStorage() {
   const fromLocal = localStorage.getItem('queue');
-  let queueStorage = JSON.parse(fromLocal);
+  localStorage.setItem('queue', JSON.stringify(checkLocaleStorage(fromLocal)));
+}
+
+function checkLocaleStorage(data) {
+  let queueStorage = JSON.parse(data);
   if (queueStorage === null) {
     queueStorage = [];
   }
-  let numb2 = [...queueStorage];
-  if (!numb2.includes(id)) {
-    numb2 = [...numb2, id];
+  let currentLocaleStorage = [...queueStorage];
+  if (!currentLocaleStorage.includes(id)) {
+    currentLocaleStorage = [...currentLocaleStorage, id];
   }
-  localStorage.setItem('queue', JSON.stringify(numb2));
-  //   const fromLocal = localStorage.getItem('queue');
-  //   const queueStorage = JSON.parse(fromLocal);
-  console.log(numb2);
+  return currentLocaleStorage;
+}
+
+function checkStorageKeys() {
+  checkWatchedKeys();
+  checkQueueKeys();
+}
+
+function checkWatchedKeys() {
+  const fromLocal = localStorage.getItem('watched');
+  let localeStorage = JSON.parse(fromLocal);
+  if (localeStorage === null) {
+    return;
+  }
+  let currentLocaleStorage = [...localeStorage];
+  if (currentLocaleStorage.includes(id)) {
+    addWatchedEl.textContent = 'Remove from watched';
+  } else {
+    addWatchedEl.textContent = 'Add to watched';
+  }
+}
+
+function checkQueueKeys() {
+  const fromLocal = localStorage.getItem('queue');
+  let localeStorage = JSON.parse(fromLocal);
+  if (localeStorage === null) {
+    return;
+  }
+  let currentLocaleStorage = [...localeStorage];
+  if (currentLocaleStorage.includes(id)) {
+    addQueueEl.textContent = 'Remove from queue';
+  } else {
+    addQueueEl.textContent = 'Add to queue';
+  }
 }
