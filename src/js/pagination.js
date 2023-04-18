c6329fdbbba6c3b62ebf26de84fdecc7eb5a8bba
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-04-18 09:04:15 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -55,7 +55,7 @@ const options = {
 };
 
 if (container) {
-  paginationPopular = new Pagination(container, options);
+  const paginationPopular = new Pagination(container, options);
   paginationPopular.on('afterMove', event => {
     if (api.query == null) {
       paginationPopularTrand = new Pagination(container, options);
