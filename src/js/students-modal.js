39b365ef81a689653eb433cf0502731d26b441b7
Nataliia Kurylo <kurilo.nata17@gmail.com>
2023-04-18 20:30:50 +0300

    Studets-modal by Olha Homes-Dovbush

---
+++ src/js/students-modal.js
@@ -0,0 +1,58 @@
+const onOpenFooterText = document.querySelector('[data-modal-students-open]');
+const onCloseModalBTN = document.querySelector('[data-members-modal-close-btn]');
+const backdroplRef = document.querySelector('.members__backdrop')
+const modalRef = document.querySelector('.members__modal')
+
+
+onOpenFooterText.addEventListener('click', openModal)
+onCloseModalBTN.addEventListener('click', closeModal)
+modalRef.addEventListener("click", (e)=> e.stopPropagation())
+
+function openModal(e) {
+    e.preventDefault()
+    backdroplRef.classList.remove('is-hidden-members');
+    window.addEventListener('keydown', escClose)
+    backdroplRef.addEventListener('click', closeModal)
+}
+  
+function closeModal(e) {
+    e.preventDefault()
+    backdroplRef.classList.add('is-hidden-members');
+    window.removeEventListener('keydown', escClose)
+    backdroplRef.removeEventListener('click', closeModal)
+  }
+
+function escClose(e) {
+    if (e.key === 'Escape') {
+        backdroplRef.classList.add('is-hidden-members');
+    }
+}
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  speed: 2500,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
+
+
+
+// openModalBtn = document.querySelector('[data-modal-students-open]');
+// closeModalBtn = document.querySelector('[data-members-modal-close-btn]');
+// membersModal = document.querySelector('.members__modal');
+// backdrop = document.querySelector('[data-members-modal]');
+// body = document.querySelector('body')
