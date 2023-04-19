e8307ee53a0df2fa2a3460b2eba4a275b15685a0
oshaleyko59 <oshaleyko@yahoo.com>
2023-04-19 01:26:30 -0500

    HTML indexes for both pages and index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import './js/test-loading-script.js';
+import './js/pagination.js';
+import './js/query-word-searching.js';
+import './js/pop-films-loading.js';
 import "./js/withScroll.js";
-import "./js/pagination.js";
+/*
+
 import "./js/modal.js";
 
-import "./js/pop-films-loading.js";
-import "./js/query-word-searching.js";
+
+
 import "./js/add-watched-queue.js";
 import "./js/students-modal.js";
-import "./js/trailer-modal.js";
-// import "./js/spinner.js";
+import "./js/trailer-modal.js"; */
+
